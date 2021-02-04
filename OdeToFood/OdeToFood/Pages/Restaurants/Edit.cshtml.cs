a1a437ed2d7b9ff4ab8eabafcd2d3dfd852928e2
charlysl <carlossl@yahoo.com>
2021-02-04 17:39:09 +0700

    Completed Module4: Creating the Restaurant Edit Page

--- OdeToFood/OdeToFood/Pages/Restaurants/Edit.cshtml.cs
+++ OdeToFood/OdeToFood/Pages/Restaurants/Edit.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using OdeToFood.Core;
 using OdeToFood.Data;
 
@@ -12,13 +13,16 @@ namespace OdeToFood.Pages.Restaurants
     public class EditModel : PageModel
     {
         private readonly IRestaurantData restaurantData;
+        private readonly IHtmlHelper htmlHelper;
 
-        public EditModel(IRestaurantData restaurantData)
+        public EditModel(IRestaurantData restaurantData, IHtmlHelper htmlHelper)
         {
             this.restaurantData = restaurantData;
+            this.htmlHelper = htmlHelper;
         }
 
         public Restaurant Restaurant { get; private set; }
+        public IEnumerable<SelectListItem> CuisineItems { get; private set; }
 
         public IActionResult OnGet(int restaurantId)
         {
@@ -27,6 +31,8 @@ namespace OdeToFood.Pages.Restaurants
             {
                 return RedirectToPage("./NotFound");
             }
+
+            CuisineItems = htmlHelper.GetEnumSelectList<CuisineType>();
             return Page();
         }
     }
