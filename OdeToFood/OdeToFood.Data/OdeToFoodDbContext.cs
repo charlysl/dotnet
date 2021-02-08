792698cfe80d61f0cf6736d412ea080155e696c0
charlysl <carlossl@yahoo.com>
2021-02-08 16:00:05 +0700

    Module 5: Implementing and Entity Framework DbContext

---
+++ OdeToFood/OdeToFood.Data/OdeToFoodDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using OdeToFood.Core;
+
+namespace OdeToFood.Data
+{
+    // Must inherit from DbContext to use Entity Framework
+    //
+    // Will automatically generate the schema.
+    public class OdeToFoodDbContext : DbContext
+    {
+        // Perform CRUD operations on Restaurant
+        //
+        // Can have dozens more entities, and also
+        // relations between them.
+        // This is covered in the EF PL courses
+        DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
